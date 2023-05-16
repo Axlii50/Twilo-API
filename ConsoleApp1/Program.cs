85e9cb52df944a64e54778d217aa8912b24f4f9b
jakub <jakub@LAPTOP-KSEMUCDD>
2023-05-16 07:54:12 +0200

    t

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -36,51 +36,51 @@ while (!access)
 }
 
 #region ttt
-string[] test = { "LIT. PIĘKNA / FANTASTYKA", "LIT. PIĘKNA / POWIEŚĆ", "LIT. PIĘKNA / FANTASTYKA", "HISTORIA / POWSZECHNA / II WOJNA ŚWIATOWA", "PEDAGOGIKA", "RELIGIE / RELIGIOZNAWSTWO", "LIT. POPULARNONAUKOWA", "RELIGIE / PUBLICYSTYKA", "LIT. FAKTU / PUBLICYSTYKA", "POLITYKA", "SZTUKA", "PARAPSYCHOLOGIA", "LIT. FAKTU / FELIETONY", "EZOTERYKA" };
-string[] test2 = { "Wicehrabia przepołowiony", "Baron drzewołaz", "Rycerz nieistniejący ", "W stronę ciemności. Rozmowy z komendantem Treblinki", "Drama. Teatr przebudzenia", "Życie codzienne w Palestynie w czasach Chrystusa", "Święta i obyczaje żydowskie", "Żydzi, świat, pieniądze", "Wściekłość i duma (dodruk 2018)", "Siła rozumu (dodruk 2018)", "Mała historia fotografii", "Kabała", "Droga Człowieka Według Nauczania Chasydów", "Nieskończone źródło twojej mocy. Klucz do pozytywnego myślenia" };
+//string[] test = { "LIT. PIĘKNA / FANTASTYKA", "LIT. PIĘKNA / POWIEŚĆ", "LIT. PIĘKNA / FANTASTYKA", "HISTORIA / POWSZECHNA / II WOJNA ŚWIATOWA", "PEDAGOGIKA", "RELIGIE / RELIGIOZNAWSTWO", "LIT. POPULARNONAUKOWA", "RELIGIE / PUBLICYSTYKA", "LIT. FAKTU / PUBLICYSTYKA", "POLITYKA", "SZTUKA", "PARAPSYCHOLOGIA", "LIT. FAKTU / FELIETONY", "EZOTERYKA" };
+//string[] test2 = { "Wicehrabia przepołowiony", "Baron drzewołaz", "Rycerz nieistniejący ", "W stronę ciemności. Rozmowy z komendantem Treblinki", "Drama. Teatr przebudzenia", "Życie codzienne w Palestynie w czasach Chrystusa", "Święta i obyczaje żydowskie", "Żydzi, świat, pieniądze", "Wściekłość i duma (dodruk 2018)", "Siła rozumu (dodruk 2018)", "Mała historia fotografii", "Kabała", "Droga Człowieka Według Nauczania Chasydów", "Nieskończone źródło twojej mocy. Klucz do pozytywnego myślenia" };
 
-int index = 0;
-foreach (string x in test)
-{
-    var tttt = await AllegroApi.GetSuggestionOfCategory(x);
-    Console.WriteLine("kategoria: " + x);
-    if (tttt.matchingCategories.Length > 0)
-    {
-        Console.WriteLine("sugerowany kategoria: " + tttt.matchingCategories[0].name);
-        Console.WriteLine(tttt.matchingCategories[0].id);
-    }
-    else
-    {
-        tttt = await AllegroApi.GetSuggestionOfCategory(test2[index]);
-        Console.WriteLine("tytuł: " + test2[index]);
+//int index = 0;
+//foreach (string x in test)
+//{
+//    var tttt = await AllegroApi.GetSuggestionOfCategory(x);
+//    Console.WriteLine("kategoria: " + x);
+//    if (tttt.matchingCategories.Length > 0)
+//    {
+//        Console.WriteLine("sugerowany kategoria: " + tttt.matchingCategories[0].name);
+//        Console.WriteLine(tttt.matchingCategories[0].id);
+//    }
+//    else
+//    {
+//        tttt = await AllegroApi.GetSuggestionOfCategory(test2[index]);
+//        Console.WriteLine("tytuł: " + test2[index]);
 
-        //moze trzeba bedzie sprawdzać czy jest leaf
-        if (tttt.matchingCategories.Length > 0)
-        {
-            Console.WriteLine("sugerowany kategoria: " + tttt.matchingCategories[0].name);
-            Console.WriteLine(tttt.matchingCategories[0].id);
-        }
-    }
-    index += 1;
-
-
-
-    Console.WriteLine();
-    if (tttt.matchingCategories.Length <= 0) continue;
-
-    var parameters = await AllegroApi.GetCategoryParameters(tttt.matchingCategories[0].id);
-
-    //    if (parameters.parameters.Length < 16) continue;
-
-    foreach (var parameter in parameters.parameters)
-    {
-        Console.WriteLine($"nazwa parametru: {parameter.name} ");
-    }
-
-    //    List<ProductParameter> param = new List<ProductParameter>();
+//        //moze trzeba bedzie sprawdzać czy jest leaf
+//        if (tttt.matchingCategories.Length > 0)
+//        {
+//            Console.WriteLine("sugerowany kategoria: " + tttt.matchingCategories[0].name);
+//            Console.WriteLine(tttt.matchingCategories[0].id);
+//        }
+//    }
+//    index += 1;
 
 
-}
+
+//    Console.WriteLine();
+//    if (tttt.matchingCategories.Length <= 0) continue;
+
+//    var parameters = await AllegroApi.GetCategoryParameters(tttt.matchingCategories[0].id);
+
+//    //    if (parameters.parameters.Length < 16) continue;
+
+//    foreach (var parameter in parameters.parameters)
+//    {
+//        Console.WriteLine($"nazwa parametru: {parameter.name} ");
+//    }
+
+//    //    List<ProductParameter> param = new List<ProductParameter>();
+
+
+//}
 
 
 
